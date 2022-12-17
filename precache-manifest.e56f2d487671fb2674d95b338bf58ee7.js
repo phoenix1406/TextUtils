3f1a0e9b1799927ddb66b051f9dc5295029755ad
phoenix1406 <varshney.c20@iiits.in>
2022-12-18 01:05:32 +0530

    Updates

---
+++ precache-manifest.e56f2d487671fb2674d95b338bf58ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9af6291c8acb00bdc8b4",
+    "url": "/TextUtils/static/css/main.86392738.chunk.css"
+  },
+  {
+    "revision": "9af6291c8acb00bdc8b4",
+    "url": "/TextUtils/static/js/main.9af6291c.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/TextUtils/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "8484ef414a40dddb27f6",
+    "url": "/TextUtils/static/js/2.8484ef41.chunk.js"
+  },
+  {
+    "revision": "423d32646782b88757ba",
+    "url": "/TextUtils/static/js/runtime~main.423d3264.js"
+  },
+  {
+    "revision": "0c8119f2842a3c5c7e5cda62d21aac72",
+    "url": "/TextUtils/index.html"
+  }
+];
